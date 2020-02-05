9c13f48f23f2756910878109785778382a0fa920
Ninad Dadmal <47479487+Ninad10code@users.noreply.github.com>
2020-02-05 14:51:12 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "function.h"
+
+void myfunction(void)
+{
+	        printf("hello Friends ! \n");
+		        return;
+}
---
+++ function.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_C
+#define FUNCTION_C
+
+void myfunction(void);
+
+#endif
